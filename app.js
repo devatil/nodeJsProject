f2dc56b1e5d91eb84ff38c9658fe7a620387c6cf
Atilla Altun <atilla.altun@ext.etiya.com>
2022-08-14 18:12:16 +0300

    lesson 2

--- app.js
+++ app.js
@@ -3,7 +3,14 @@ import express from 'express';
 const app = express();
 
 const port = 3000;
-app.get("/", (req, res) => res.send("Hello Worldddd!"));
+//ejs is a templating engine
+app.set('view engine', 'ejs');
+
+//static files
+app.use(express.static('public'));
+
+app.get("/", (req, res) => res.render("index"));
+app.get("/about", (req, res) => res.render("about"));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
